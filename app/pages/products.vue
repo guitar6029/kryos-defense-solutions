98d0a98579bc1bf303410de79e980dbe0bc80b6e
jsdev305 <jsdev305@gmail.com>
2025-11-30 20:45:26 -0500

    added mk-i vidoe and image , updated products ui

--- app/pages/products.vue
+++ app/pages/products.vue
@@ -1,3 +1,12 @@
+<script lang="ts" setup>
+import Absolute from "~/components/Wrapper/Absolute.vue";
+import KryosPanel from "~/components/Panels/KryosPanel.vue";
+import KryosCard from "~/components/Cards/KryosCard.vue";
+import MKIVideo from "~/assets/video/mki.mp4";
+import MKIModel from "~/assets/img/mk-i-no-bg.png";
+import MKIModelBg from "~/assets/img/mk-i.jpg";
+</script>
+
 <!-- <main>
   [1] Page Title + Intro
   [2] Product Families Overview
@@ -9,43 +18,55 @@
 </main> -->
 
 <template>
-  <div class="flex flex-col gap-2">
-    <h1>Kryos Platform Catalog</h1>
-    <p>
-      Kryos Defense Solutions develops autonomous platforms engineered for
-      long-duration deployment in high-risk, high-value environments. Each
-      system is built on a shared core architecture, with configurations
-      tailored to specific operational demands.
-    </p>
-    <p>
-      This catalog summarizes the platforms currently available for procurement,
-      starting with our flagship ground unit.
-    </p>
+  <div class="flex flex-col relative gap-4 min-h-screen">
+    <Absolute extra-class="top-20 left-1/2 -translate-x-1/2 w-200">
+      <KryosPanel :option="11" />
+    </Absolute>
+
+    <section
+      class="flex flex-col relative items-center justify-center gap-4 min-h-[50vh]"
+    >
+      <h1 class="section-title">Kryos Platform Catalog</h1>
+      <p class="text-2xl max-w-2xl">
+        Kryos Defense Solutions develops autonomous platforms engineered for
+        long-duration deployment in high-risk, high-value environments. Each
+        system is built on a shared core architecture, with configurations
+        tailored to specific operational demands.
+      </p>
+      <p class="text-2xl max-w-2xl">
+        This catalog summarizes the platforms currently available for
+        procurement, starting with our flagship ground unit.
+      </p>
+    </section>
     <!-- Product Families Overview -->
-    <section class="flex flex-col gap-2">
-      <h2>Platform Families</h2>
-      <div class="flex flex-col gap-2">
-        <h3>Ground Platforms (Active)</h3>
-        <p>
-          Autonomous, multi-terrain units for patrol, interdiction, and close
-          asset protection.
-        </p>
+    <section class="flex flex-col items-center min-h-screen gap-6 mb-50">
+      <h2 class="section-title">Platform Families</h2>
+      <div class="flex flex-wrap items-center justify-center gap-6">
+        <KryosCard
+          title="Ground Platforms (Active)"
+          text="Autonomous, multi-terrain units for patrol, interdiction, and close
+        asset protection."
+        />
+
+        <KryosCard
+          title="Aerial Recon Platforms (In Development)"
+          text="Lightweight, persistent-surveillance drones designed for layered
+        situational awareness."
+        />
+
+        <KryosCard
+          title="Infrastructure Integration Systems (In Development)"
+          text="Embedded systems for transit networks, industrial facilities, and
+        critical infrastructure nodes."
+        />
+
+        <KryosCard
+          title="Ground Platforms (Active)"
+          text="Autonomous, multi-terrain units for patrol, interdiction, and close
+        asset protection."
+        />
       </div>
-      <div class="flex flex-col gap-2">
-        <h3>Aerial Recon Platforms (In Development)</h3>
-        <p>
-          Lightweight, persistent-surveillance drones designed for layered
-          situational awareness.
-        </p>
-      </div>
-      <div class="flex flex-col gap-2">
-        <h3>Infrastructure Integration Systems (In Development)</h3>
-        <p>
-          Embedded systems for transit networks, industrial facilities, and
-          critical infrastructure nodes.
-        </p>
-      </div>
-      <p>
+      <p class="text-(--kryos-warn) text-4xl">
         Note: Certain platform configurations are restricted and available only
         under classified procurement channels.
       </p>
@@ -53,17 +74,57 @@
     <!-- END Product Families Overview -->
 
     <!-- Kryos MK-I Flagship Spotlight -->
-    <section class="flex flex-col gap-2">
-      <h2>Kryos MK-I Autonomous Tactical Drone</h2>
-      <p>
-        The Kryos MK-I is a multi-terrain ground platform designed for dense
-        urban, industrial, and subterranean environments. Six-degree mobility,
-        redundant sensor arrays, and modular payload interfaces allow MK-I units
-        to operate continuously under adverse conditions.
-      </p>
+    <section class="flex flex-col items-center gap-2 relative">
+      <div class="w-full h-screen bg-(--kryos-mk-bg) relative pb-60">
+        <h2
+          class="absolute top-10 left-1/2 -translate-x-1/2 font-orbitron font-extrabold text-[10rem] z-10"
+        >
+          KRYOS MK-I
+        </h2>
+        <img
+          :src="MKIModel"
+          alt="MKI Model with background"
+          class="w-500 absolute left-1/2 -translate-x-1/2 -bottom-24 lg:-bottom-32"
+        />
+        <Absolute
+          extra-class="absolute bottom-10 left-25 -rotate-90 origin-bottom-left"
+        >
+          <div class="border-4 p-2">
+            <span class="font-orbitron font-extrabold text-8xl"
+              >KRYOS MK-I</span
+            >
+          </div>
+        </Absolute>
+      </div>
+
+      <div class="relative flex gap-2 mt-50 border h-125">
+        <div
+          class="absolute bottom-0 left-0 -rotate-90 origin-bottom-left w-125 h-25 border translate-x-px flex flex-col items-center justify-center"
+        >
+          <span class="font-orbitron font-extrabold text-6xl">KRYOS MK-I</span>
+        </div>
+
+        <div class="flex flex-col justify-center gap-2 p-2 max-w-2xl">
+          <h2 class="text-6xl">Kryos MK-I Autonomous Tactical Drone</h2>
+          <p class="text-2xl">
+            The Kryos MK-I is a multi-terrain ground platform designed for dense
+            urban, industrial, and subterranean environments. Six-degree
+            mobility, redundant sensor arrays, and modular payload interfaces
+            allow MK-I units to operate continuously under adverse conditions.
+          </p>
+        </div>
+      </div>
+      <!-- 
+      <div class="max-w-300 absolute right-0 top-10">
+        <img :src="MKIModel" alt="Kryos MKI model" />
+      </div> -->
+
+      <video controls muted class="w-full">
+        <source :src="MKIVideo" type="video/webm" />
+      </video>
 
       <!-- blocks -->
-      <div class="flex flex-col gap-2">
+      <!-- <div class="flex flex-col gap-2">
         <h3>Core Specifications</h3>
         <ul>
           <li>Configuration: Six-legged autonomous ground platform</li>
@@ -79,8 +140,8 @@
           </li>
           <li>Payload: Two modular hardpoints, configurable per deployment</li>
         </ul>
-      </div>
-      <div class="flex flex-col gap-2">
+      </div> -->
+      <!-- <div class="flex flex-col gap-2">
         <h3>System Architecture</h3>
         <p>
           MK-I units are built on a hardened core chassis with isolated compute,
@@ -94,19 +155,21 @@
           allowing low-latency autonomous adjustments to terrain, threat
           posture, and local conditions.
         </p>
-      </div>
+      </div> -->
 
-      <div class="flex flex-col gap-2">
-        <!-- internal anchor -->
+      <!-- <div class="flex flex-col gap-2">
+      
         <p>View deployment modes and profiles</p>
-        <NuxtLink to="/contact">Request detailed technical brief</NuxtLink>
-      </div>
+        <NuxtLink to="/contact" class="link"
+          >Request detailed technical brief</NuxtLink
+        >
+      </div> -->
     </section>
     <!-- END Kryos MK-I Flagship Spotlight -->
 
     <!-- Capabilities & Operating Modes -->
-    <section class="flex flex-col gap-2">
-      <h2>Capabilities & Operating Modes</h2>
+    <!-- <section class="flex flex-col gap-2">
+      <h2 class="section-title">Capabilities & Operating Modes</h2>
       <div class="flex flex-col gap-2">
         <h3>Platform Capabilities</h3>
         <ul>
@@ -172,12 +235,12 @@
           </p>
         </div>
       </div>
-    </section>
+    </section> -->
     <!-- END Capabilities & Operating Modes -->
 
     <!-- Deployment Profiles-->
-    <section class="flex flex-col gap-2">
-      <h2>Deployment Profiles</h2>
+    <!-- <section class="flex flex-col gap-2">
+      <h2 class="section-title">Deployment Profiles</h2>
       <div class="grid grid-cols-3 gap-4">
         <div class="flex flex-col gap-2">
           <h3>Tier-1 Corporate Campus Security</h3>
@@ -210,12 +273,12 @@
           </p>
         </div>
       </div>
-    </section>
+    </section> -->
     <!-- END Deployment Profiles -->
 
     <!-- Roadmap / Future Platforms -->
-    <section class="flex flex-col gap-2">
-      <h2>Upcoming Platforms</h2>
+    <!-- <section class="flex flex-col gap-2">
+      <h2 class="section-title">Upcoming Platforms</h2>
       <div class="flex flex-col gap-2">
         <h3>Kryos MK-II (Concept Stage)</h3>
         <p>
@@ -237,24 +300,28 @@
           redundancy, and complex perimeter management.
         </p>
       </div>
-    </section>
+    </section> -->
     <!-- END Roadmap / Future Platforms -->
 
     <!-- Procurement & Integration CTA -->
-    <section class="flex flex-col gap-2">
-      <h2>Procurement & Systems Integration</h2>
-      <p>
+    <!-- <section
+      class="flex flex-col items-center justify-center gap-2 min-h-screen"
+    >
+      <h2 class="section-title">Procurement & Systems Integration</h2>
+      <p class="text-4xl max-w-2xl">
         Kryos platforms are available under controlled procurement channels for
         qualified defense, security, and critical infrastructure clients. Each
         deployment is preceded by a joint assessment phase to align platform
         capabilities with operational requirements and regulatory frameworks.
       </p>
-      <p>
+      <p class="text-2xl max-w-2xl">
         For procurement inquiries, integration planning, or technical briefings,
         contact the Kryos deployment team.
       </p>
-      <NuxtLink to="/contact">Contact Kryos Deployment Team</NuxtLink>
-    </section>
+      <NuxtLink to="/contact" class="link"
+        >Contact Kryos Deployment Team</NuxtLink
+      >
+    </section> -->
     <!-- end Procurement & Integration CTA -->
   </div>
 </template>
