106954f8b944096f2ad65b884d6d05a05b8593dc
jsdev305 <jsdev305@gmail.com>
2025-12-05 17:44:09 -0500

    removed extra mb-50

--- app/components/Products/ProductCTA.vue
+++ app/components/Products/ProductCTA.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex flex-col text-center gap-2 mb-50">
+  <div class="flex flex-col text-center gap-2">
     <p class="text-center text-4xl">{{ text }}</p>
     <NuxtLink :to="to" class="link">
       {{ linkLabel }}
