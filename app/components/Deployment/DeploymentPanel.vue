e8db0d5cc0ff26d5acf94088222a02a66c44ce81
jsdev305 <jsdev305@gmail.com>
2025-12-05 17:27:45 -0500

    updated ui

---
+++ app/components/Deployment/DeploymentPanel.vue
@@ -0,0 +1,101 @@
+<script lang="ts" setup>
+import Absolute from "../Wrapper/Absolute.vue";
+import AirDeployment from "~/assets/img/swarm_drones.jpg";
+import LandDeployment from "~/assets/img/land_deployment.jpg";
+import SeaDeployment from "~/assets/img/sea_deployment.jpg";
+import DigitalDeployment from "~/assets/img/digital_deployment.jpg";
+export type DeploymentType = "air" | "land" | "sea" | "digital";
+
+defineProps<{
+  deployment: DeploymentType;
+}>();
+
+const selectedDeployment: Record<
+  DeploymentType,
+  { title: string; description: string; imgSrc: string }
+> = {
+  air: {
+    title: "AIR",
+    description: "ISR, overwatch, and interception.",
+    imgSrc: AirDeployment,
+  },
+
+  land: {
+    title: "LAND",
+    description: "Patrol, interdiction, and ground support.",
+    imgSrc: LandDeployment,
+  },
+
+  sea: {
+    title: "SEA",
+    description: "Maritime patrol and subsurface recon.",
+    imgSrc: SeaDeployment,
+  },
+
+  digital: {
+    title: "DIGITAL",
+    description: "Cyber defense and signal intelligence.",
+    imgSrc: DigitalDeployment,
+  },
+};
+</script>
+
+<template>
+  <div
+    class="flex w-full h-150 flex-row items-center gap-8 bg-(--kryos-bg) p-4 relative z-1"
+  >
+    <div
+      class="absolute flex items-center gap-4 left-1/2 -translate-x-1/2 animate-brief"
+    >
+      <span class="font-bold text-(--kryos-warn) font-orbitron text-[6rem]"
+        >[{{ selectedDeployment[deployment].title }}]</span
+      >
+
+      <div class="relative w-150">
+        <div
+          class="text-[2.5rem] flex-1 text-(--kryos-text-high) font-bold px-6 py-4 z-1"
+        >
+          <span class="uppercase">{{
+            selectedDeployment[deployment].description
+          }}</span>
+        </div>
+      </div>
+    </div>
+
+    <!-- PANEL WRAPPER -->
+    <Absolute
+      class="w-250 h-150 left-1/2 -translate-x-1/2 flex items-center justify-center -z-1 opacity-50"
+    >
+      <svg
+        class="w-full h-auto"
+        viewBox="0 0 670 307"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <!-- Your panel shape -->
+          <clipPath :id="`platform_${deployment}`">
+            <path
+              d="M0.5 114.5V76L6.13576 57.5L23.5 0.5H66H188L205.5 8H483.5L504.5 0.5H539L581 42.5V76H565V114.5H581L603.229 163L650.208 265.5L669 306.5H647L616.5 295H551L524 306.5H407.5L381 295H216L195.5 306.5H155.5L137.336 284L117.5 265.5L129.5 258L59 163H39.6536L0.5 114.5Z"
+            />
+          </clipPath>
+        </defs>
+
+        <image
+          :href="selectedDeployment[deployment].imgSrc"
+          width="670"
+          height="307"
+          preserveAspectRatio="xMidYMid slice"
+          :clip-path="`url(#platform_${deployment})`"
+        />
+
+        <path
+          d="M0.5 114.5V76L6.13576 57.5L23.5 0.5H66H188L205.5 8H483.5L504.5 0.5H539L581 42.5V76H565V114.5H581L603.229 163L650.208 265.5L669 306.5H647L616.5 295H551L524 306.5H407.5L381 295H216L195.5 306.5H155.5L137.336 284L117.5 265.5L129.5 258L59 163H39.6536L0.5 114.5Z"
+          fill="none"
+          stroke="var(--kryos-accent)"
+          stroke-width="2"
+        />
+      </svg>
+    </Absolute>
+  </div>
+</template>
--- app/components/SectorDeployments.vue
+++ app/components/SectorDeployments.vue
@@ -4,9 +4,7 @@ import KryosPanel from "./Panels/KryosPanel.vue";
 import SwarmDrones from "~/assets/img/swarm_drones.jpg";
 </script>
 <template>
-  <div
-    class="flex flex-col min-h-screen gap-2 relative bg-(--kryos-bg-alt) z-1"
-  >
+  <div class="flex flex-col min-h-screen relative bg-(--kryos-bg-alt) z-1">
     <Absolute
       extra-class="bottom-1/2 translate-y-1/2 left-1/2 -translate-x-1/2 sm:w-100 lg:w-400 -z-1 opacity-20"
     >
@@ -15,80 +13,11 @@ import SwarmDrones from "~/assets/img/swarm_drones.jpg";
 
     <Splitter right-text="SECTOR // DEPLOYMENTS" />
 
-    <div class="flex flex-col items-center gap-12 text-[4rem] mt-20">
-      <div
-        class="flex w-full h-150 flex-row items-center gap-8 bg-(--kryos-bg) p-4 relative z-1"
-      >
-        <div
-          class="absolute flex items-center gap-4 left-1/2 -translate-x-1/2 animate-brief"
-        >
-          <span class="font-bold text-(--kryos-warn) font-orbitron text-[6rem]"
-            >[AIR]</span
-          >
-          <div
-            class="text-[2.5rem] flex-1 text-(--kryos-text-high) font-bold bg-black/55 px-6 py-2"
-          >
-            <span>High-altitude ISR / interception / overwatch</span>
-          </div>
-        </div>
-
-        <!-- PANEL WRAPPER -->
-        <Absolute
-          class="w-250 h-150 left-1/2 -translate-x-1/2 flex items-center justify-center -z-1 opacity-50"
-        >
-          <svg
-            class="w-full h-auto"
-            viewBox="0 0 670 307"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <!-- Your panel shape -->
-              <clipPath id="platformAir">
-                <path
-                  d="M0.5 114.5V76L6.13576 57.5L23.5 0.5H66H188L205.5 8H483.5L504.5 0.5H539L581 42.5V76H565V114.5H581L603.229 163L650.208 265.5L669 306.5H647L616.5 295H551L524 306.5H407.5L381 295H216L195.5 306.5H155.5L137.336 284L117.5 265.5L129.5 258L59 163H39.6536L0.5 114.5Z"
-                />
-              </clipPath>
-            </defs>
-
-            <image
-              :href="SwarmDrones"
-              width="670"
-              height="307"
-              preserveAspectRatio="xMidYMid slice"
-              clip-path="url(#platformAir)"
-            />
-
-            <path
-              d="M0.5 114.5V76L6.13576 57.5L23.5 0.5H66H188L205.5 8H483.5L504.5 0.5H539L581 42.5V76H565V114.5H581L603.229 163L650.208 265.5L669 306.5H647L616.5 295H551L524 306.5H407.5L381 295H216L195.5 306.5H155.5L137.336 284L117.5 265.5L129.5 258L59 163H39.6536L0.5 114.5Z"
-              fill="none"
-              stroke="var(--kryos-accent)"
-              stroke-width="2"
-            />
-          </svg>
-        </Absolute>
-      </div>
-
-      <div class="flex flex-row items-center gap-8">
-        <span class="font-bold text-(--kryos-warn) font-orbitron">[LAND]</span>
-        <span class="text-[3.5rem]"
-          >High-altitude ISR / interception / overwatch</span
-        >
-      </div>
-      <div class="flex flex-row items-center gap-8">
-        <span class="font-bold text-(--kryos-warn) font-orbitron">[SEA]</span>
-        <span class="text-[3.5rem]"
-          >High-altitude ISR / interception / overwatch</span
-        >
-      </div>
-      <div class="flex flex-row items-center gap-8">
-        <span class="font-bold text-(--kryos-warn) font-orbitron"
-          >[DIGITAL]</span
-        >
-        <span class="text-[3.5rem]"
-          >High-altitude ISR / interception / overwatch</span
-        >
-      </div>
+    <div class="flex flex-col items-center text-[4rem] mt-20">
+      <DeploymentPanel deployment="air" />
+      <DeploymentPanel deployment="land" />
+      <DeploymentPanel deployment="sea" />
+      <DeploymentPanel deployment="digital" />
     </div>
   </div>
 </template>
