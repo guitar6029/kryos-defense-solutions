d771c1a8be0ead3d225659cb2419f9cb6692b9ee
jsdev305 <jsdev305@gmail.com>
2025-12-05 19:02:03 -0500

    updated ui

--- app/components/KryosHighlightMKI.vue
+++ app/components/KryosHighlightMKI.vue
@@ -42,29 +42,29 @@ import KryosPanel from "./Panels/KryosPanel.vue";
     <Splitter right-text="SECTOR // PLATFORM" />
 
     <h2 class="section-title">Flagship Platform</h2>
-    <div class="flex items-center gap-2">
+    <div class="flex flex-col xl:flex-row items-center gap-2">
       <div class="flex flex-col items-center gap-2 flex-1">
-        <img :src="MKI" alt="Kryos MK-I Prototype" class="w-200" />
+        <img :src="MKI" alt="Kryos MK-I Prototype" class="min-w-200" />
         <h3
           class="text-6xl text-(--kryos-text-high) font-orbitron uppercase w-full text-center"
         >
           Kryos MK-I
         </h3>
       </div>
-      <div class="flex flex-col items-center gap-6 max-w-4xl">
+      <div class="flex flex-col gap-6 max-w-4xl">
         <p class="text-4xl">
           The Kryos MK-I is a multi-terrain autonomous drone designed for
           high-density urban, industrial, and subterranean environments. With
           six-degrees-of-freedom mobility and a hardened sensor array, MK-I
           units maintain operational integrity where conventional systems fail.
         </p>
-        <ul class="text-4xl list-disc font-bold flex flex-col gap-4 max-w-2xl">
+        <ul class="text-4xl font-bold flex flex-col gap-4 max-w-4xl">
           <li>Multi-surface traversal (floor, wall, overhead structures)</li>
           <li>Redundant sensor clusters (thermal, LIDAR, acoustic mapping)</li>
           <li>Modular payload hardpoints</li>
           <li>Encrypted command and control channel</li>
         </ul>
-        <NuxtLink to="/products/mk-1" class="link"
+        <NuxtLink to="/products/mk-1" class="link w-fit"
           >View full technical specification</NuxtLink
         >
       </div>
