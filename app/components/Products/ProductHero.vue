9f9792eb816c721322c398f3db3c8eb9036912c9
jsdev305 <jsdev305@gmail.com>
2025-12-07 22:41:28 -0500

    updated ui, added new Product Hero image

--- app/components/Products/ProductHero.vue
+++ app/components/Products/ProductHero.vue
@@ -1,37 +1,14 @@
 <script lang="ts" setup>
-import Wall from "~/assets/img/wall-panel.jpg";
+import Kryos_Catalog from "~/assets/img/kryos_catalog.png";
 </script>
 
 <template>
   <section class="relative flex min-h-screen items-center justify-center">
     <!-- Background image -->
     <img
-      :src="Wall"
-      alt="gray wall"
-      class="absolute inset-0 w-full h-full object-cover -z-10 opacity-40"
+      :src="Kryos_Catalog"
+      alt="Kryos Catalog"
+      class="absolute inset-0 w-full h-full object-cover -z-10"
     />
-
-    <!-- Optional dark overlay to boost contrast -->
-    <div class="absolute inset-0 bg-black/40 z-0"></div>
-
-    <!-- Content -->
-    <div class="relative z-10 flex flex-col items-center gap-6 px-4">
-      <h1 class="section-title px-8 py-4">Kryos Platform Catalog</h1>
-
-      <div
-        class="relative text-4xl flex flex-col gap-10 bg-black/30 px-6 py-4 max-w-4xl"
-      >
-        <p>
-          Kryos Defense Solutions develops autonomous platforms engineered for
-          long-duration deployment in high-risk, high-value environments. Each
-          system is built on a shared core architecture, with configurations
-          tailored to specific operational demands.
-        </p>
-        <p>
-          This catalog summarizes the platforms currently available for
-          procurement, starting with our flagship ground unit.
-        </p>
-      </div>
-    </div>
   </section>
 </template>
