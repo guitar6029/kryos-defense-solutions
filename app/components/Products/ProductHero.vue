23d8fe1492f0a68ae2a7d57348d2961999c30cdb
jsdev305 <jsdev305@gmail.com>
2025-12-12 18:45:43 -0500

    updated ui ProductHero, modified height

--- app/components/Products/ProductHero.vue
+++ app/components/Products/ProductHero.vue
@@ -3,12 +3,25 @@ import Kryos_Catalog from "~/assets/img/kryos_catalog.png";
 </script>
 
 <template>
-  <section class="relative flex min-h-screen items-center justify-center">
+  <section class="relative flex h-[50vh] items-center justify-center">
     <!-- Background image -->
     <img
       src="~/assets/img/kryos_catalog.png"
       alt="Kryos Catalog"
       class="absolute inset-0 w-full h-full object-cover opacity-70"
     />
+
+    <svg
+      width="423"
+      height="83"
+      viewBox="0 0 423 83"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.5 28L38.5 6.5H69.5L102.5 0.5H350.5L360 6.5H391L422 28L415.5 64L350.5 82.5H69.5L0.5 64V28Z"
+        stroke="black"
+      />
+    </svg>
   </section>
 </template>
