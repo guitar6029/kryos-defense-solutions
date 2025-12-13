585302c784239a7cdd0d74f772d3d7e3eb3e85ba
jsdev305 <jsdev305@gmail.com>
2025-12-12 20:20:46 -0500

    updatd ui

--- app/components/Products/ProductHero.vue
+++ app/components/Products/ProductHero.vue
@@ -1,9 +1,9 @@
-<script lang="ts" setup>
-import Kryos_Catalog from "~/assets/img/kryos_catalog.png";
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <section class="relative flex h-[50vh] items-center justify-center">
+  <section
+    class="relative flex h-screen items-center justify-center overflow-hidden"
+  >
     <!-- Background image -->
     <img
       src="~/assets/img/kryos_catalog.png"
@@ -11,17 +11,48 @@ import Kryos_Catalog from "~/assets/img/kryos_catalog.png";
       class="absolute inset-0 w-full h-full object-cover opacity-70"
     />
 
-    <svg
-      width="423"
-      height="83"
-      viewBox="0 0 423 83"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+    <div
+      class="absolute top-0 right-0 z-10 w-full md:w-1/2 shrink-0 h-full flex flex-col items-center gap-2"
     >
-      <path
-        d="M0.5 28L38.5 6.5H69.5L102.5 0.5H350.5L360 6.5H391L422 28L415.5 64L350.5 82.5H69.5L0.5 64V28Z"
-        stroke="black"
-      />
-    </svg>
+      <div
+        class="text-2xl xl:text-6xl z-10 flex flex-col gap-2 relative top-30 font-bold"
+      >
+        <span>KRYOS</span>
+        <span>SYSTEM CATALOG</span>
+        <span class="text-xl">RESTRICTED ACCESS</span>
+      </div>
+
+      <div class="sm:block md:hidden absolute inset-0">
+        <svg
+          class="w-full h-full"
+          viewBox="0 0 1667 899"
+          fill="#7f4105"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1666.2 898.5V0.5H863.203H0.202881L545.203 242.5L1029.7 356L674.203 772.5L1029.7 898.5H1666.2Z"
+            stroke="#041520"
+            stroke-width="4"
+          />
+        </svg>
+      </div>
+
+      <div class="hidden md:block absolute inset-0 opacity-90">
+        <svg
+          class="w-full h-full"
+          viewBox="0 0 587 785"
+          fill="#7f4105"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0.568359 86L68.5684 0.5H585.568V784H68.5684L34.5684 693.5L213.568 543L114.068 443.5V321.5L49.0684 213.5L0.568359 86Z"
+            stroke="#041520"
+            stroke-width="4"
+          />
+        </svg>
+      </div>
+    </div>
   </section>
 </template>
