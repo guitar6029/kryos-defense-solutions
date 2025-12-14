be603a55ee3defe21a4b604899bd6bc3f6b518cb
jsdev305 <jsdev305@gmail.com>
2025-12-14 11:12:51 -0500

    updated the modal ui

--- app/layouts/systems.vue
+++ app/layouts/systems.vue
@@ -29,8 +29,8 @@ function handleEnter() {
         <h1 class="font-bold text-5xl">KRYOS SYSTEMS INTERFACE</h1>
       </template>
 
-      <div class="flex flex-col text-4xl">
-        <div class="flex flex-col gap-12 max-w-4xl">
+      <div class="flex flex-col text-2xl">
+        <div class="flex flex-col gap-4 max-w-4xl">
           <p>
             Accessing this interface initiates the Kryos internal systems layer.
           </p>
@@ -39,17 +39,25 @@ function handleEnter() {
             autonomous platforms and tactical infrastructure.
           </p>
 
-          <div class="flex flex-row items-center gap-2 font-bold uppercase">
+          <div class="h-[.1rem] bg-(--kryos-accent)"></div>
+
+          <div
+            class="flex flex-row items-center gap-2 font-bold uppercase text-[1rem]"
+          >
             <span class="text-(--kryos-warn)">[Module]</span>
             <span>Systems</span>
           </div>
 
-          <div class="flex flex-row items-center gap-2 font-bold uppercase">
+          <div
+            class="flex flex-row items-center gap-2 font-bold uppercase text-[1rem]"
+          >
             <span class="text-(--kryos-warn)">[Release Channel]</span>
             <span>Internal</span>
           </div>
 
-          <div class="flex flex-row items-center gap-2 font-bold uppercase">
+          <div
+            class="flex flex-row items-center gap-2 font-bold uppercase text-[1rem]"
+          >
             <span class="text-(--kryos-warn)">[Version]</span>
             <span>2.4.8</span>
           </div>
