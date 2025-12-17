aa55db47677dfb0c37859f33ccf70719b92b84fa
jsdev305 <jsdev305@gmail.com>
2025-12-17 14:48:55 -0500

    created a seprated secton for svg accents and text

---
+++ app/components/Products/FullViewProductAccents.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import Absolute from "../Wrapper/Absolute.vue";
+import KryosPanel from "../Panels/KryosPanel.vue";
+defineProps<{
+  text: string;
+}>();
+</script>
+
+<template>
+  <Absolute extra-class="absolute top-0 w-75  right-0 rotate-180">
+    <KryosPanel stroke="var(--kryos-accent)" :option="6" />
+  </Absolute>
+
+  <Absolute extra-class="absolute top-0 w-50  left-0">
+    <KryosPanel stroke="var(--kryos-accent)" :option="6" />
+  </Absolute>
+
+  <Absolute
+    extra-class="absolute bottom-10 left-20  -rotate-90 origin-bottom-left"
+  >
+    <div class="p-2 border-b-6 border-b-(--kryos-warn)">
+      <span class="font-orbitron font-extrabold text-xl text-nowrap">{{
+        text
+      }}</span>
+    </div>
+  </Absolute>
+</template>
