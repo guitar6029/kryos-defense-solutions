d734fd00e6d58a065b96e0c7eac5513a02af5906
jsdev305 <jsdev305@gmail.com>
2025-12-17 14:51:20 -0500

    updated max width value

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <Nav />
     <main class="flex justify-center min-h-screen overflow-x-hidden">
-      <div class="w-full max-w-[3000px] flex flex-col gap-2">
+      <div class="w-full max-w-8xl flex flex-col gap-2 cursor-default">
         <slot />
       </div>
     </main>
