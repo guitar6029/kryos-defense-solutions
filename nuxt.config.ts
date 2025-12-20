61617064ac57121fe2eb85238c1a3288f6ff7548
jsdev305 <jsdev305@gmail.com>
2025-12-20 17:37:39 -0500

    added tanstack query , added query modules,

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,5 +17,44 @@ export default defineNuxtConfig({
       { name: "Orbitron", weights: [400, 500, 700] },
     ],
   },
-  modules: ["@nuxt/fonts", "@nuxt/icon", "@pinia/nuxt", '@nuxt/ui'],
+  modules: [
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "@nuxt/ui",
+    "@peterbud/nuxt-query",
+  ],
+  nuxtQuery: {
+    /**
+     * Specify which Vue Query composables to auto-import
+     * Default: `false`, set to `true` to auto-import all Vue Query composables
+     */
+    autoImports: ["useQuery", "useMutation"],
+
+    // Enable/disable Nuxt DevTools integration (default: true)
+    devtools: true,
+
+    /**
+     * These are the same options as the QueryClient
+     * from @tanstack/vue-query, which will be passed
+     * to the QueryClient constructor
+     * More details: https://tanstack.com/query/v5/docs/reference/QueryClient
+     */
+
+    queryClientOptions: {
+      defaultOptions: {
+        queries: {
+          // for example disable refetching on window focus
+          refetchOnWindowFocus: false,
+          staleTime: 60_000,
+          // refetchInterval: 5000
+          // or change the default refetch interval
+          //refetchInterval: 5000,
+        },
+      },
+    },
+  },
+  runtimeConfig: {
+    jsonServerBaseUrl: "http://localhost:3001",
+  },
 });
