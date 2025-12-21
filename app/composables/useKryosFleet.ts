fbc4f70d2daa0c7d5b3069c3a9f6c309fd6626eb
jsdev305 <jsdev305@gmail.com>
2025-12-20 20:36:07 -0500

    changed dir

--- app/composables/useKryosFleet.ts
+++ app/composables/useKryosFleet.ts
@@ -1,13 +1,8 @@
 import { useQuery } from "#imports";
-import { getKryosFleet } from "~/utils/api/fleet";
-
 export const useKryosFleet = () => {
-  console.log("useKryosFleet invoked");
-  console.trace();
-
   return useQuery({
     queryKey: ["fleet"],
-    queryFn: getKryosFleet,
+    queryFn: () => $fetch('/api/fleet'),
     staleTime: 60_000,
     refetchOnWindowFocus: false,
   });
--- app/utils/api/fleet.ts
+++
@@ -1,9 +0,0 @@
-export const getKryosFleet = async () => {
-  console.log(
-    "getKryosFleet called. client?",
-    import.meta.client,
-    "server?",
-    import.meta.server
-  );
-  return await $fetch("/api/fleet");
-};
