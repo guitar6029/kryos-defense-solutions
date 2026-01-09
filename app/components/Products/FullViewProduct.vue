e139dd7e9716c5c2dfba9ad253d9357dabcd60f0
jsdev305 <jsdev305@gmail.com>
2026-01-09 16:30:25 -0500

    updated SummaryProduct ui

--- app/components/Products/FullViewProduct.vue
+++ app/components/Products/FullViewProduct.vue
@@ -31,7 +31,7 @@ const selectedModel: Record<Platform, { title: string; imageSrc: string }> = {
 
 <template>
   <div
-    class="w-full min-h-[50vh] group flex flex-col gap-2 lg:flex-row items-center border-2 border-(--kryos-accent)/40 rounded-2xl trns justify-around relative hover:bg-(--kryos-bg-alt)"
+    class="w-full p-12 min-h-[50vh] group flex flex-col gap-2 md:flex-row items-center border-2 border-(--kryos-accent)/40 rounded-2xl trns justify-around relative hover:bg-(--kryos-bg-alt)"
   >
     <div class="relative trns group min-w-100 max-w-150">
       <img
@@ -43,14 +43,14 @@ const selectedModel: Record<Platform, { title: string; imageSrc: string }> = {
         extra-class="relative bottom-20 left-0 -rotate-90 origin-bottom-left"
       >
         <div class="p-2 border-b-2 w-fit border-b-(--kryos-warn)">
-          <span class="font-orbitron font-extrabold text-xl text-nowrap">
+          <span class="font-orbitron font-extrabold text-sm md:text-xl text-nowrap">
             {{ selectedModel[model].title }}
           </span>
         </div>
       </Absolute>
     </div>
 
-    <div class="flex flex-col gap-4 relative">
+    <div class="flex flex-col gap-4 w-87.5 md:max-w-4xl">
       <SummaryProduct :model="model" />
       <ProductCTA
         to="/products/mk-1"
--- app/components/Products/SummaryProduct.vue
+++ app/components/Products/SummaryProduct.vue
@@ -11,12 +11,12 @@ const selectedModel: Record<
   MK: {
     title: "KRYOS MK-1",
     fullTitle: "Kryos MK-I Autonomous Tactical Drone",
-    summary: `The Kryos MK-I is a multi-terrain ground platform designed for dense urban, industrial, and subterranean environments. Six-degree mobility, redundant sensor arrays, and modular payload interfaces allow MK-I units to operate continuously under adverse conditions.`,
+    summary: `The [Kryos MK-I] is a multi-terrain ground platform designed for dense urban, industrial, and subterranean environments. Six-degree mobility, redundant sensor arrays, and modular payload interfaces allow MK-I units to operate continuously under adverse conditions.`,
   },
   EX: {
     title: "KRYOS EX-1",
     fullTitle: "Kryos EX-1 Tactical ExoFrame",
-    summary: `The Kryos EX-1 is a powered exoskeleton platform designed for high-load,
+    summary: `The [Kryos EX-1] is a powered exoskeleton platform designed for high-load,
         high-risk operations. Humanoid articulation, assisted lift systems, and
         a hardened sensor spine allow a single operator to move, stabilize, and
         handle mass that would normally require a full team and heavy equipment.`,
@@ -25,12 +25,12 @@ const selectedModel: Record<
 </script>
 
 <template>
-  <div class="relative flex">
-    <div class="flex flex-col gap-2 pl-2 pt-4 min-w-2xl max-w-4xl">
-      <h2 class="text-4xl">
+  <div class="flex">
+    <div class="flex flex-col gap-2 pl-2 pt-4">
+      <h2 class="text-2xl">
         {{ selectedModel[model].fullTitle }}
       </h2>
-      <p class="text-2xl max-w-2xl">
+      <p class="text-2xl w-full md:max-w-2xl">
         {{ selectedModel[model].summary }}
       </p>
     </div>
