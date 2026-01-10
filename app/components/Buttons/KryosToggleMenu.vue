6e92a21c9e40fdc619fe36d4609b07b7ab1841ed
jsdev305 <jsdev305@gmail.com>
2026-01-09 19:39:30 -0500

    added fixed property to the menu for mobile view

--- app/components/Buttons/KryosToggleMenu.vue
+++ app/components/Buttons/KryosToggleMenu.vue
@@ -10,11 +10,12 @@ defineProps<{
 
 <template>
   <!-- header -->
-  <div class="w-full flex items-center justify-end p-4 z-100">
-    <button v-if="isMobile" @click="emit('toggle-menu')">
+  <div v-if="isMobile" class="fixed top-5 right-5 z-100">
+    <button @click="emit('toggle-menu')">
       <Icon
         class="trns hover:text-(--kryos-text-high)"
         name="tdesign:menu-filled"
+        size="20"
       />
     </button>
   </div>
